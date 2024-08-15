515fb62210c58c9f9d1ef6e824adafe272c849fe
FridayMerchant <riley.yates12@gmail.com>
2024-08-15 06:26:35 +0000

    Week 4 Scripts

--- week4/IpAddressesOnly.sh
+++ week4/IpAddressesOnly.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Execute the ipinfo.sh script and show output
+#ipinfo.sh script and show output
 ip_info_output=$(./ipinfo.sh)
 
-# Use sed with -n flag and p command to filter and display only IP Addresses
+#use sed with -n flag and p command to filter and display only IP Addresses
 echo "$ip_info_output" | sed -n '/IP Address:/p'
 
